6148f0b6ad2ff62d97f37826fca2d793a494332a
veteran1 <hamou.aras@gmail.com>
2020-07-07 20:20:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ dotnet ef migrations script -o ./Database/XtraUpload.Database.Migrations/script.
 echo "The sql script [script.sql] has been successfully generated, path: ./Database/XtraUpload.Database.Migrations/"
 echo "7. updating db..."
 dotnet ef database update initCommit -p ./Database/XtraUpload.Database.Migrations -s XtraUpload.WebApp
-buildDir="/var/wwwroot"
-echo "8. Moving build directory to " + $buildDir
+buildDir="$HOME/wwwroot"
+echo "8. Moving build directory to " $buildDir
 mkdir -p $buildDir; mv ./XtraUpload.WebApp/bin/Release/netcoreapp3.1/publish/* $_
 rm ./XtraUpload.WebApp/bin/*
 echo "9. starting web server..."
