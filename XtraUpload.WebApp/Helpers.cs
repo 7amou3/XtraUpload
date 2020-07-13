4c4969586e2d28ad93512056371d68d4d0a66a99
ARRAS, HAMMOU <hammou.arras.external@atos.net>
2020-07-13 23:42:32 +0200

    remove comment

--- XtraUpload.WebApp/Helpers.cs
+++ XtraUpload.WebApp/Helpers.cs
@@ -13,7 +13,7 @@ namespace XtraUpload.WebApp
             services.Configure<T>(section);
             services.AddTransient<IWritableOptions<T>>(provider =>
             {
-                var environment = provider.GetService<IWebHostEnvironment>(); // IWebHostEnvironment
+                var environment = provider.GetService<IWebHostEnvironment>();
                 var options = provider.GetService<IOptionsMonitor<T>>();
                 return new WritableOptions<T>(environment.ContentRootFileProvider, options, section.Key, file);
             });
