bd1a37593e6c2a4add3d206c2218a0e8cb684b9c
7amou3 <993610+7amou3@users.noreply.github.com>
2020-09-20 15:45:10 +0200

    add project tpl for storage servers

---
+++ Services/StorageManager/XtraUpload.StorageManager.Host/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SixLabors.ImageSharp.Web.DependencyInjection;
+using System;
+using tusdotnet;
+using XtraUpload.StorageManager.Service;
+using XtraUpload.StorageManager.Common;
+using XtraUpload.Domain.Infra;
+
+namespace XtraUpload.StorageManager.Host
+{
+    public static class StartUp
+    {
+        public static void UseFileManager(this IApplicationBuilder app)
+        {
+            app.UseTus(httpContext =>
+            {
+                if (httpContext.Request.Path.StartsWithSegments(new PathString("/avatarupload")))
+                {
+                    return httpContext.RequestServices.GetService<AvatarUploadService>().CreateTusConfiguration();
+                }
+                else if (httpContext.Request.Path.StartsWithSegments(new PathString("/fileupload")))
+                {
+                    return httpContext.RequestServices.GetService<FileUploadService>().CreateTusConfiguration();
+                }
+                else return null;
+            });
+        }
+        public static void AddFileManager(this IServiceCollection services, IConfiguration config)
+        {
+            // Registre services
+            services.AddSingleton<AvatarUploadService>();
+            services.AddSingleton<FileUploadService>();
+            services.AddImageSharp();
+
+            
+            // Background jobs
+            services.AddHostedService<ExpiredFilesCleanupService>();
+            
+            // Health check
+            services.AddHealthChecks().AddCheck<FileStoreHealthCheck>("Storage Permissions");
+
+            // Upload Options
+            IConfigurationSection uploadSection = config.GetSection(nameof(UploadOptions));
+            services.Configure<UploadOptions>(uploadSection);
+            services.ConfigureWritable<UploadOptions>(uploadSection);
+        }
+    }
+}
