5de62c5ba70d36134f575623b18118a24112d908
7amou3 <993610+7amou3@users.noreply.github.com>
2020-12-02 21:10:44 +0100

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("soft-valley.net")]
+[assembly: AssemblyCompany("soft-valley.net")] 
 [assembly: AssemblyProduct("XtraUpload")]
-[assembly: AssemblyCopyright("soft-valley © XtraUpload 2020")]
+[assembly: AssemblyCopyright("soft-valley Â© XtraUpload 2020")]
 
 // Version information for an assembly consists of the following four values:
 //
